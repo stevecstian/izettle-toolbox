6058d2237265d829b474c817259ba49820818e89
Mikael Löthman <mikael.lothman@izettle.com>
2013-10-09 15:43:29 +0200

    Additions to ValueChecks
    Adds the follwoing to ValueChecks:
    
    * notEmpty
    * notDefined
    * define

--- izettle-java/src/main/java/com/izettle/java/ValueChecks.java
+++ izettle-java/src/main/java/com/izettle/java/ValueChecks.java
@@ -36,6 +36,33 @@ public class ValueChecks {
 	}
 
 	/**
+	 * The inverse of <code>areDefined</code>.
+	 *
+	 * @param object Object.
+	 * @return True any of the specified objects is null, false otherwise.
+	 */
+	public static boolean notDefined(Object... object) {
+		return !areDefined(object);
+	}
+
+	/**
+	 * Utility method for shorter notation of possible null check before assignment, eg: String s2 = s1 != null ? s1 :
+	 * ""; can instead be written as: String s2 = define(s1, "");
+	 *
+	 * @param <T> The type of the subject
+	 * @param <S> The type for the fallback value, must be same type, or subclass of T
+	 * @param subject check if this is null
+	 * @param fallback value to use if subject was null
+	 * @return the subject if not null, fallback otherwise
+	 */
+	public static <T, S extends T> T define(T subject, S fallback) {
+		if (isDefined(subject)) {
+			return subject;
+		}
+		return fallback;
+	}
+
+	/**
 	 * Checks if the given object is empty. Here empty is defined as: <ul> <li>a null {@link Object}</li> <li>an empty {@link String}</li>
 	 * <li>an empty {@link java.lang.reflect.Array}</li> <li>an empty {@link java.util.Collection}</li> <li>an empty {@link java.util.Map}</li> </ul>
 	 *
@@ -59,5 +86,15 @@ public class ValueChecks {
 			return ((Map<?, ?>) o).isEmpty();
 		}
 		return false;
+	}
+
+	/**
+	 * The inverse of <code>isEmpty()</code>.
+	 *
+	 * @param o Object under test.
+	 * @return True if the object is not empty, false otherwise.
+	 */
+	public static boolean notEmpty(Object o) {
+		return !isEmpty(o);
 	}
 }
