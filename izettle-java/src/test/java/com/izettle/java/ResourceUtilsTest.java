aa6e33f696c400e5bce6c8e1ec839c8d7f8fb3bd
Niklas Kullberg <niklas@izettle.com>
2013-11-25 13:08:16 +0100

    Added methods to ResourceUtils for class relative resource lookup

--- izettle-java/src/test/java/com/izettle/java/ResourceUtilsTest.java
+++ izettle-java/src/test/java/com/izettle/java/ResourceUtilsTest.java
@@ -1,7 +1,8 @@
 package com.izettle.java;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static com.izettle.java.ResourceUtils.getResourceAsBytes;
+import static com.izettle.java.ResourceUtils.getResourceAsStream;
+import static org.fest.assertions.api.Assertions.assertThat;
 
 import java.io.InputStream;
 import org.junit.Test;
@@ -9,21 +10,39 @@ import org.junit.Test;
 public class ResourceUtilsTest {
 	@Test
 	public void testGetResourceBytes() throws Exception {
-		byte[] bytes = ResourceUtils.getResourceAsBytes("resourceloadertestfile.txt");
-		assertTrue(bytes.length > 0);
+		byte[] bytes = getResourceAsBytes("resourceloadertestfile.txt");
+		assertThat(new String(bytes))
+				.isNotEmpty()
+				.isEqualTo("test file for resource loading");
 
-		bytes = ResourceUtils.getResourceAsBytes("/resourceloadertestfile.txt");
-		assertTrue(bytes.length > 0);
+		bytes = getResourceAsBytes("/resourceloadertestfile.txt");
+		assertThat(new String(bytes))
+				.isNotEmpty()
+				.isEqualTo("test file for resource loading");
 	}
 
 	@Test
 	public void testGetResourceAsStream() throws Exception {
-		InputStream inputStream = ResourceUtils.getResourceAsStream("resourceloadertestfile.txt");
-		assertNotNull(inputStream);
+		InputStream inputStream = getResourceAsStream("resourceloadertestfile.txt");
+		assertThat(inputStream).isNotNull();
 		inputStream.close();
 
-		inputStream = ResourceUtils.getResourceAsStream("/resourceloadertestfile.txt");
-		assertNotNull(inputStream);
+		inputStream = getResourceAsStream("/resourceloadertestfile.txt");
+		assertThat(inputStream).isNotNull();
 		inputStream.close();
+	}
+
+	@Test
+	public void testGetResourceAsBytesWithContextClass() throws Exception {
+		byte[] resource = getResourceAsBytes(ResourceUtilsTest.class, "resourcewithcontexttestfile.txt");
+		assertThat(new String(resource))
+				.isNotEmpty()
+				.isEqualTo("test file for resource loading given a context class");
+	}
+
+	@Test
+	public void testGetResourceAsStreamWithContextClass() throws Exception {
+		InputStream stream = getResourceAsStream(ResourceUtilsTest.class, "resourcewithcontexttestfile.txt");
+		assertThat(stream).isNotNull();
 	}
 }
