b8dcb471f5968941604139b5d0b445f611563e97
Oskar Wiksten <oskar@izettle.com>
2014-01-07 13:32:05 +0100

    Bugfix: Renamed a method but forgot to update test

--- izettle-java/src/test/java/com/izettle/java/DateFormatCreatorTest.java
+++ izettle-java/src/test/java/com/izettle/java/DateFormatCreatorTest.java
@@ -10,8 +10,8 @@ public class DateFormatCreatorTest {
 	private final Date dateToVerify = new Date(1387920896123L); // 24 Dec 2013, 22:34:56.123 (UTC+1)
 
 	@Test
-	public void shouldFormatDatesAsISO8601InUTC() throws Exception {
-		String formattedDate = DateFormatCreator.createISO8601Formatter().format(dateToVerify);
+	public void shouldFormatDatesAsRFC3339InUTC() throws Exception {
+		String formattedDate = DateFormatCreator.createRFC3339Formatter().format(dateToVerify);
 		assertEquals("2013-12-24T21:34:56.123+0000", formattedDate);
 	}
 }
