0692d0b27d7ddc4f248d1a2dd8c11aa2a0a3e745
Oskar Wiksten <oskar@izettle.com>
2014-01-09 11:40:08 +0100

    Methods for creating alternative uuids based on existing uuids
    * Adds method `createAlternative` to `UUIDFactory`, that can produce another uuid based on an original one, in a deterministic fashion.
    * Adds method `uuidToByteArray` to `UUIDFactory`, that produces a byte representation of a uuid.

--- izettle-java/src/test/java/com/izettle/java/UUIDFactoryTest.java
+++ izettle-java/src/test/java/com/izettle/java/UUIDFactoryTest.java
@@ -2,6 +2,7 @@ package com.izettle.java;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -37,5 +38,33 @@ public class UUIDFactoryTest {
 		assertFalse(s.isEmpty());
 		assertTrue(s.length() < originalString.length());
 		assertEquals("za7VbYcSQU2zRgGQXQAm_g", s);
+	}
+
+	@Test
+	public void uuidsCanBeDeconstructedToByteArrays() {
+		// We should be able to create a byte array from a uuid string.
+		byte[] bytes1 = UUIDFactory.uuidToByteArray(UUIDFactory.createAsString());
+		assertEquals(16, bytes1.length);
+
+		byte[] bytes2 = UUIDFactory.uuidToByteArray("notProducedByCreateAsString");
+		assertEquals(27, bytes2.length);
+
+		byte[] bytes3 = UUIDFactory.uuidToByteArray("123456789");
+		assertEquals(9, bytes3.length);
+	}
+
+	@Test
+	public void canCreateAlternativeVersionsOfUUIDs() {
+		// Arrange
+		String uuid = UUIDFactory.createAsString();
+		assertEquals(22, uuid.length());
+
+		// Act
+		String maskedUUID = UUIDFactory.createAlternative(uuid);
+
+		// Assert
+		assertNotNull(maskedUUID);
+		assertEquals(22, maskedUUID.length());
+		assertNotEquals(uuid, maskedUUID);
 	}
 }
