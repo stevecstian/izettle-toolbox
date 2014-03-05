018df787650c50a5d585bea0d06f884865f0a932
Anders Jansson <anders@izettle.com>
2014-03-05 14:58:48 +0100

    LanguageId: add iw, old iso code for hebrew.

--- izettle-java/src/main/java/com/izettle/java/LanguageId.java
+++ izettle-java/src/main/java/com/izettle/java/LanguageId.java
@@ -6,5 +6,5 @@ public enum LanguageId {
 	fo, fr, fy, ga, gd, gl, gn, gu, ha, he, hi, hr, hu, hy, ia, ie, ik, in, is, ja, jv, ka, kk, kl, km, kn, ko, ks,
 	ku, ky, la, ln, lo, lt, lv, mg, mi, mk, ml, mn, mo, mr, ms, mt, my, na, ne, nl, oc, om, or, pa, pl, ps, pt, qu,
 	rm, rn, ro, ru, rw, sa, sd, sg, sh, si, sk, sl, sm, sn, so, sq, sr, ss, st, su, sw, ta, te, tg, th, ti, tk, tl,
-	tn, to, tr, ts, tt, tw, uk, ur, uz, vi, vo, wo, xh, yi, yo, zh, zu, da, en, es, fi, it, nb, sv
+	tn, to, tr, ts, tt, tw, uk, ur, uz, vi, vo, wo, xh, yi, yo, zh, zu, da, en, es, fi, it, nb, sv, iw
 }
