ccc69130514d66b5bd9c35f1d5006c05945310c8
renato.athaydes <renato.de.athaydes@izettle.com>
2018-03-26 17:00:57 +0200

    Improved test for SchemaVersionUpdater.

--- izettle-cassandra-datastax/src/test/java/com/izettle/cassandra/SchemaVersionUpdaterWithDatastaxDriverTest.java
+++ izettle-cassandra-datastax/src/test/java/com/izettle/cassandra/SchemaVersionUpdaterWithDatastaxDriverTest.java
@@ -1,36 +1,30 @@
 package com.izettle.cassandra;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.izettle.cassandra.SchemaVersionUpdaterWithDatastaxDriver.TABLE_NAME;
 
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.KeyspaceMetadata;
 import com.datastax.driver.core.Session;
+import com.datastax.driver.core.exceptions.InvalidQueryException;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Date;
-import org.apache.thrift.transport.TTransportException;
+import java.util.Set;
+import me.prettyprint.cassandra.service.CassandraHost;
+import org.cassandraunit.CassandraUnit;
 import org.cassandraunit.DataLoader;
 import org.cassandraunit.dataset.yaml.ClassPathYamlDataSet;
-import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class SchemaVersionUpdaterWithDatastaxDriverTest {
-    final private static String TABLE_NAME = "schema_scripts_version";
 
-    @BeforeClass
-    public static void beforeClass() throws InterruptedException, IOException, TTransportException {
-        EmbeddedCassandraServerHelper.startEmbeddedCassandra();
-    }
+    @Rule
+    public CassandraUnit cassandraUnit = new CassandraUnit(new ClassPathYamlDataSet("dataset-empty.yaml"));
 
-    @Before
-    public void before() {
-        EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
-    }
-
-    @Test(expected = IllegalStateException.class)
+    @Test(expected = InvalidQueryException.class)
     public void dieIfTableSchemaIsIncorrect() throws IOException, URISyntaxException {
         load("dataset-legacy.yaml");
 
@@ -53,14 +47,41 @@ public class SchemaVersionUpdaterWithDatastaxDriverTest {
         SchemaVersionUpdaterWithDatastaxDriver updater = new SchemaVersionUpdaterWithDatastaxDriver(session);
         updater.applyFromResources(SchemaVersionUpdaterWithDatastaxDriverTest.class, "migrations");
 
-        KeyspaceMetadata keyspaceMetadata =
-            session.getCluster().getMetadata().getKeyspace(session.getLoggedKeyspace());
+        KeyspaceMetadata keyspaceMetadata = session.getCluster().getMetadata()
+            .getKeyspace(session.getLoggedKeyspace());
+
         assertThat(keyspaceMetadata.getTable("galaxies")).isNull();
+        assertThat(keyspaceMetadata.getTable("planets")).isNotNull();
+    }
+
+    @Test
+    public void applyScriptsIfNotYetDone() throws IOException, URISyntaxException {
+        load("dataset-empty.yaml");
+
+        Session session = getSession();
+
+        SchemaVersionUpdaterWithDatastaxDriver updater = new SchemaVersionUpdaterWithDatastaxDriver(session);
+        updater.applyFromResources(SchemaVersionUpdaterWithDatastaxDriverTest.class, "migrations");
+
+        KeyspaceMetadata keyspaceMetadata = session.getCluster().getMetadata()
+            .getKeyspace(session.getLoggedKeyspace());
+
+        assertThat(keyspaceMetadata.getTable("galaxies")).isNotNull();
+        assertThat(keyspaceMetadata.getTable("planets")).isNotNull();
     }
 
     private void load(String dataSetLocation) {
-        DataLoader dataLoader = new DataLoader("TestCluster", "localhost:9171");
+        CassandraHost host = getCassandraHost();
+        DataLoader dataLoader = new DataLoader("TestCluster", host.getUrl());
         dataLoader.load(new ClassPathYamlDataSet(dataSetLocation));
+    }
+
+    private CassandraHost getCassandraHost() {
+        Set<CassandraHost> hosts = cassandraUnit.cluster.getConnectionManager().getHosts();
+        if (hosts.isEmpty()) {
+            throw new IllegalStateException("Cannot run test, no Cassandra hosts available");
+        }
+        return hosts.iterator().next();
     }
 
     private static Session getSession() {
